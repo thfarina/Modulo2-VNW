b32f3c725193c58f4f66d31ca4265cba193e316b
thfarina <thalitafarina3@gmail.com>
2022-01-28 22:07:56 -0300

    finalizado exercício 01

--- 01 - Estrutura de dados/script.js
+++ 01 - Estrutura de dados/script.js
@@ -1,13 +1,18 @@
 // Crie uma variável que receba um nome;
-
+let name = "Vovó Juju"
 // Crie uma variável que receba um número;
-
+let number = 30
 // Crie uma variável com o ano atual e subtraia pelo número que você guardou;
-
+let year = 2022
+let calc = year - number
+console.log(calc);
 // Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
-
+calc = year + number
+console.log(calc);
 //Mostre no console a mensagem "Estou mexendo no console.log";
-
+console.log("Estou mexendo no console.log");
 // Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
-
+let quartaFeira = Boolean(true)
+console.log(quartaFeira);
 // Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log(typeof quartaFeira);
