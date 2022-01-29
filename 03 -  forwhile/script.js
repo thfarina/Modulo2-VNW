30c3cb30aa7d7d426e8bbb3b7d37a0e3cff77568
thfarina <thalitafarina3@gmail.com>
2022-01-28 22:55:34 -0300

    finalizado ex 3

--- 03 -  forwhile/script.js
+++ 03 -  forwhile/script.js
@@ -1,13 +1,20 @@
-*SUBA O DESAFIO NO GITHUB E ENTREGUE O LINK DO CÓDIGO AQUI, (NÃO PRECISA COLOCAR ONLINE)
-
-
 //1 - Criar um loop que conte de 1 até 10 usando FOR
-
-
+for (var i = 1; i < 11; i++){
+  console.log(i);
+}
 //2 - Criar um loop que conte de 10 até 1 usando WHILE
-
-
+while (i > 1) {
+  i -= 1
+  console.log(i);
+}
 //3 - Criar um loop que conte todos os números ímpares de 1 até 100
-
-
+while (i < 99) {
+  console.log(i);
+  i += 2
+}
 //4- Criar um loop que conte todos os números pares de 0 a 100
+i = 0
+while (i < 100) {
+  console.log(i);
+  i += 2
+}
